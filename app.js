c075f9a0167ee7e0ecbbe5cbb0d42ddfc181af93
Karen Gonzalez <kagoncaoca@gmail.com>
2022-07-14 15:35:45 -0300

    server-routes

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const path = require('path');
+const port = 3030;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'login.html')));
+app.get('/productCart', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'productCart.html')));
+app.get('/productDetail', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'productDetail.html')));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'register.html')));
+
+
+
+app.listen(port, () => console.log('Servidor corriendo en http://localhost:' + port));
