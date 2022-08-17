914edf5d9c2b0c2a549ccb964aa9cb32505199e2
MarcusZ93 <it.marcuscs@gmail.com>
2022-08-17 17:39:28 -0300

    Creando method override y enrutando vista agregar producto

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,16 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+
+const productos = require('../data/vinos.json');
+const usuarios = require('../data/usuarios.json');
+
+
 module.exports = {
     productDetail : (req, res) => {
+
+        const productos = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'vinos.json')));
+
+        const {id} = req.params;
+        let producto = productos.find(producto => producto.id === +id)
+
         return res.render('productDetail', {
             title : 'Detalle',
             stylesheets: 'productDetail.css'
-            
+            , producto
         });
     },
+
+    add : (req, res) => {
+
+        return res.render('productAdd', {
+            title : 'Agregar producto'
+            
+        });
+
+    }
+    
+    ,
+
     //Falta crear el controlador para el productCart
     productCart : (req, res) => {
         return res.render('productCart', {
             title : 'Carrito',
             stylesheets: 'productCart.css'
+        
+        
         })
+
+
+
     }
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -2,29 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 
-const {productDetail, productCart} = require('../controllers/productController')
+const {productDetail, productCart , add} = require('../controllers/productController')
 
 /* /products*/
 
 router
     .get('/productDetail', productDetail)
     .get('/productCart', productCart)
-
+    .get('/add',add)
     
 
-    /products (GET) Listado de productos
 
-    /products/create (GET) Formulario de creación de productos
-
-    /products/:id (GET) Detalle de un producto particular
-
-    /products (POST) Acción de creación (a donde se envía el formulario)
-
-    /products/:id/edit (GET) Formulario de edición de productos
-
-    /products/:id (PUT) Acción de edición (a donde se envía el formulario):
-
-    /products/:id (DELETE) :Acción de borrado
-
+   
 
 module.exports = router;
